08b3b4ccc881d6684df95c10d3229e26653f21ec
Aaron Cheng <aaronchenghs@gmail.com>
2023-09-21 13:15:26 -0500

    Schedule page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import FlowChart from "./components/Flowchart/flowchart.component";
+import Schedule from "./components/Schedule/schedule.component";
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<FlowChart />} />
-        {/* <Route path="vita" element={<Vita />} /> */}
+        <Route path="schedule" element={<Schedule />} />
       </Routes>
     </div>
   );
---
+++ src/components/Schedule/schedule.component.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import styles from "./schedule.module.scss";
+
+const Schedule = (): JSX.Element => {
+  return (
+    <Fragment>
+      <div className={styles.Schedule}>This is the Schedule view</div>
+    </Fragment>
+  );
+};
+export default Schedule;
