845aec9dbacbeddcee57f6a4d388bbfd31c3df72
Aaron Cheng <acheng8@lsu.edu>
2023-09-21 23:52:50 -0500

    top bar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import FlowChart from "./components/Flowchart/flowchart.component";
 import Schedule from "./components/Schedule/schedule.component";
+import TopBar from "./components/TopBar/topbar.component";
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
+      <TopBar />
       <Routes>
-        <Route path="/" element={<FlowChart />} />
+        <Route path="/" element={<FlowChart />} index />
         <Route path="schedule" element={<Schedule />} />
       </Routes>
     </div>
