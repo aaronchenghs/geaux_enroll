f446a8ad1b76d1bf21c0297f8ca0836091b4d21e
Aaron Cheng <acheng8@lsu.edu>
2023-09-29 16:37:41 -0500

    Structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import { Route, Routes } from "react-router-dom";
 import FlowChart from "./components/Flowchart/flowchart.component";
 import Schedule from "./components/Schedule/schedule.component";
 import TopBar from "./components/TopBar/topbar.component";
+import BottomBar from "./components/BottomBar/bottombar.component";
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
       <TopBar />
-      <Routes>
-        <Route path="/" element={<FlowChart />} index />
-        <Route path="schedule" element={<Schedule />} />
-      </Routes>
+      <div className="focusedContent">
+        <Routes>
+          <Route path="/" element={<FlowChart />} index />
+          <Route path="schedule" element={<Schedule />} />
+        </Routes>
+      </div>
+      <BottomBar />
     </div>
   );
 };
