590c9d3428cb9abb88d0c929dfd6ed6a27c9eccb
Aaron Cheng <aaronchenghs@gmail.com>
2023-10-06 18:19:36 -0500

    Progress bar

--- src/components/TopBar/DegreeTopBarContent/degreetopbarcontent.component.tsx
+++ src/components/TopBar/DegreeTopBarContent/degreetopbarcontent.component.tsx
@@ -1,12 +1,35 @@
 import React, { Fragment } from "react";
-import SegmentedProgressBar from "../ProgressBar/segmentedprogressbar.component";
+import SegmentedProgressBar, {
+  segment,
+} from "../ProgressBar/segmentedprogressbar.component";
 //styles
 import styles from "./degreetopbarcontent.module.scss";
+
+const sampleSegments: segment[] = [
+  {
+    id: "1",
+    label: "wow",
+    color: "#008000",
+    value: 30,
+    tooltip: "This is hours complete",
+  },
+  {
+    id: "2",
+    label: "wow",
+    color: "#FFC300",
+    value: 15,
+    tooltip: "This is hours scheduled.",
+  },
+];
 
 const DegreeTopBarContent = (): JSX.Element => {
   return (
     <Fragment>
-      {/* <SegmentedProgressBar segments={[]} max={0} /> */}Degree Top Bar
+      <div className={styles.progressBarContainer}>
+        {" "}
+        {/** max here should be the total hours needed to graduate */}
+        <SegmentedProgressBar segments={sampleSegments} max={100} />
+      </div>
     </Fragment>
   );
 };
--- src/components/TopBar/ProgressBar/segmentedprogressbar.component.tsx
+++ src/components/TopBar/ProgressBar/segmentedprogressbar.component.tsx
@@ -1,14 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./segmentedprogressbar.module.scss";
 
-interface segment {
+export interface segment {
   id: string;
   label: string;
   color: string;
   value: number;
-  tooltip: "This is the initial phase.";
-  isActive: true;
-  onClick: () => void;
+  tooltip: string;
 }
 
 interface Props {
@@ -17,20 +15,81 @@ interface Props {
 }
 
 const SegmentedProgressBar = ({ segments }: Props): JSX.Element => {
+  const [tooltip, setTooltip] = useState<string | null>(null);
+  const [tooltipPos, setTooltipPos] = useState<{ x: number; y: number } | null>(
+    null,
+  );
+
+  const showTooltip = (
+    e: React.MouseEvent<HTMLDivElement>,
+    segment: segment,
+  ): void => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setTooltip(segment.tooltip);
+    setTooltipPos({ x: rect.x + rect.width / 2, y: rect.bottom });
+  };
+
+  const hideTooltip = (): void => {
+    setTooltip(null);
+    setTooltipPos(null);
+  };
+
   return (
-    <div className={styles.progressBarContainer}>
+    <div
+      className={styles.progressBarContainer}
+      style={{ position: "relative" }}
+    >
       {segments.map((segment, index) => (
         <div
           key={index}
           className={styles.progressBarSegment}
-          style={{
-            flexBasis: `${segment.value}%`,
-            backgroundColor: segment.color,
-          }}
+          style={
+            {
+              flexBasis: `${segment.value}%`,
+              backgroundColor: segment.color,
+              "--darkenedColor": darkenColor(segment.color, 30),
+            } as React.CSSProperties
+          }
+          onMouseOver={(e): void => showTooltip(e, segment)}
+          onMouseOut={hideTooltip}
         ></div>
       ))}
+
+      {tooltip && tooltipPos && (
+        <div
+          className={styles.tooltip}
+          style={{ left: `${tooltipPos.x}px`, top: `${tooltipPos.y}px` }}
+        >
+          {tooltip}
+        </div>
+      )}
     </div>
   );
+};
+
+/**
+ * darkenColor(string, number)
+ *
+ * @param color Color as a hex code
+ * @param percent % to darken by
+ * @returns New darkened hex code
+ */
+export const darkenColor = (color: string, percent: number): string => {
+  const num = parseInt(color.replace("#", ""), 16);
+  const amt = Math.round(2.55 * percent);
+  let R = (num >> 16) - amt;
+  let G = ((num >> 8) & 0x00ff) - amt;
+  let B = (num & 0x0000ff) - amt;
+
+  // Ensure values are bounded by 0 and 255
+  R = R < 0 ? 0 : R > 255 ? 255 : R;
+  G = G < 0 ? 0 : G > 255 ? 255 : G;
+  B = B < 0 ? 0 : B > 255 ? 255 : B;
+
+  return `#${((1 << 24) + (R << 16) + (G << 8) + B)
+    .toString(16)
+    .slice(1)
+    .toUpperCase()}`;
 };
 
 export default SegmentedProgressBar;
