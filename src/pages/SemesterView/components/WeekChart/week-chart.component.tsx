38372535e66347756ea543cca019d454f3317e8a
AdamASchulte <adamaschulte@yahoo.com>
2023-10-12 22:31:27 -0500

    initial grid

--- src/pages/SemesterView/components/WeekChart/week-chart.component.tsx
+++ src/pages/SemesterView/components/WeekChart/week-chart.component.tsx
@@ -8,7 +8,10 @@ import styles from "./week-chart.module.scss";
 export const WeekChart = (): JSX.Element => {
   return (
     <>
-      <h2 className={styles.title}> Schedule </h2>
+    <h2 className={styles.title}> Schedule </h2>
+      <div className={styles.grid}>
+        <div className={styles.item}>1</div>
+      </div>
     </>
   );
 };
