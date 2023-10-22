2ba3c5ac7433f9b1fcf6ea5b82bd4a0284ab6160
Christian <cdubs06@gmail.com>
2023-10-22 17:32:13 -0500

    Gave Names and descriptions to all B TechElectives

--- src/models/database/TechElectivesB.ts
+++ src/models/database/TechElectivesB.ts
@@ -3,9 +3,10 @@ import { Course, Department } from "../course";
 // GROUP B ELECTIVES
 
 export const ART2050 = new Course({
-  name: "",
+  name: "Digital Art I",
   code: 2050,
   department: Department.ART,
+  description: `This course covers the technologies and production processes used to successfully create and complete digital art and design projects, from brainstorming of concepts, to scanning, importing, creating, printing, executing and presenting works.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -13,9 +14,10 @@ export const ART2050 = new Course({
 });
 
 export const ART2055 = new Course({
-  name: "",
+  name: "Digital Art II",
   code: 2055,
   department: Department.ART,
+  description: `Introductory work in digital animation and multimedia applications.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -23,9 +25,10 @@ export const ART2055 = new Course({
 });
 
 export const ART2210 = new Course({
-  name: "",
+  name: "Creative Coding",
   code: 2210,
   department: Department.ART,
+  description: `An introduction to computer programming for creative applications focusing on languages useful for web development.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -33,9 +36,10 @@ export const ART2210 = new Course({
 });
 
 export const ART2220 = new Course({
-  name: "",
+  name: "Moving Image",
   code: 2220,
   department: Department.ART,
+  description: `Introduction to digital video production and editing systems; concepts will include basic compositing and motion graphics.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -43,9 +47,10 @@ export const ART2220 = new Course({
 });
 
 export const ART2230 = new Course({
-  name: "",
+  name: "Virtual Space",
   code: 2230,
   department: Department.ART,
+  description: `This course offers an introduction to modeling and animation using three-dimensional objects and spaces in a virtual environment.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -53,9 +58,10 @@ export const ART2230 = new Course({
 });
 
 export const ART2551 = new Course({
-  name: "",
+  name: "Typography for Visual Communications",
   code: 2551,
   department: Department.ART,
+  description: `Historical overview of type and letter forms; introduction to professional typography in print and digital environments; primary focus will be applications to contemporary communications.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -63,9 +69,10 @@ export const ART2551 = new Course({
 });
 
 export const ART4020 = new Course({
-  name: "",
+  name: "Special Topics in Studio",
   code: 4020,
   department: Department.ART,
+  description: `In this course, students explore meaning through interactivity while pursuing independent and group projects that use digital media to create an interactive experience.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -73,9 +80,10 @@ export const ART4020 = new Course({
 });
 
 export const ART4050 = new Course({
-  name: "",
+  name: "Digital Art III",
   code: 4050,
   department: Department.ART,
+  description: `Students will immerse themselves in the intricacies of digital animation, refining their skills in character development, scene design, and dynamic motion. With an emphasis on both technical proficiency and artistic expression, the course offers a comprehensive look at the processes behind creating captivating animated narratives`,
   prereqs: [],
   grade: null,
   section: null,
@@ -83,9 +91,10 @@ export const ART4050 = new Course({
 });
 
 export const ART4055 = new Course({
-  name: "",
+  name: "Divital Art IV",
   code: 4055,
   department: Department.ART,
+  description: `Building on prior foundational and intermediate knowledge, students will master advanced techniques in digital imaging, delve into the intricacies of video production, and hone their animation skills to create seamless and compelling narratives.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -93,9 +102,10 @@ export const ART4055 = new Course({
 });
 
 export const ART4230 = new Course({
-  name: "",
+  name: "Virtual Space & Motion",
   code: 4230,
   department: Department.ART,
+  description: `The course focuses on group projects and honing skills in the 3D pipeline. Particular attention is placed on time-base narratives and creating digitally animated shorts using 3D graphics. Students learn about 3D painting, 3D sculpting, motion capture, particle systems, and compositing.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -103,9 +113,10 @@ export const ART4230 = new Course({
 });
 
 export const ART4240 = new Course({
-  name: "",
+  name: "Topics in Digital Art: Virtual Production",
   code: 4240,
   department: Department.ART,
+  description: `An introductory level project-based course open to all majors. Virtual Production refers to the process of combining live action with virtual environments, digital props, and VFX in real-time.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -113,9 +124,10 @@ export const ART4240 = new Course({
 });
 
 export const ART4290 = new Course({
-  name: "",
+  name: "Digital Art Synthesis",
   code: 4290,
   department: Department.ART,
+  description: `Digital Art Synthesis is an advanced studio focused on the completion and presentation of a large scale digital media project.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -123,9 +135,10 @@ export const ART4290 = new Course({
 });
 
 export const ART4550 = new Course({
-  name: "",
+  name: "Digital Imaging for Visual Communications",
   code: 4550,
   department: Department.ART,
+  description: `Basic exploration of digital photographic technology and its application in communications; topics include: scanning, image processing and manipulation, digital filtering, and imaging peripherals; emphasis on emerging technology and preparing images for multimedia applications.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -133,9 +146,10 @@ export const ART4550 = new Course({
 });
 
 export const ART4059 = new Course({
-  name: "",
+  name: "Digital Media Capstone",
   code: 4059,
   department: Department.ART,
+  description: `Culminating capstone project experience requiring interdisciplinary teams to prototype a digital media work or application.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -143,9 +157,10 @@ export const ART4059 = new Course({
 });
 
 export const ART4560 = new Course({
-  name: "",
+  name: "Interactive Media for Visual Communications",
   code: 4560,
   department: Department.ART,
+  description: `Basic application of interactive digital technology: design and application of Internet-based communications, hypermedia language, virtual reality, sound and visual synchronization, communications standards, emerging technologies and multimedia; special focus on the study and application of interactive multimedia theory.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -153,9 +168,10 @@ export const ART4560 = new Course({
 });
 
 export const ART7250 = new Course({
-  name: "",
+  name: "Digital Art Praxis",
   code: 7250,
   department: Department.ART,
+  description: `This advanced course emphasizes a collaborative approach, where participants engage in collective research, brainstorming, and development processes. Throughout the course, students will be tasked with conceptualizing and implementing experimental projects that challenge the boundaries of conventional digital media.`,
   prereqs: [],
   grade: null,
   credits: 3,
@@ -163,9 +179,10 @@ export const ART7250 = new Course({
 });
 
 export const ART7255 = new Course({
-  name: "",
+  name: "Digital Art Seminar",
   code: 7255,
   department: Department.ART,
+  description: `Creative and theoretical project critique, discussion of contemporary issues and research-based investigations in digital media.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -173,9 +190,10 @@ export const ART7255 = new Course({
 });
 
 export const ARTH4480 = new Course({
-  name: "",
+  name: "Video Art and Theory",
   code: 4480,
   department: Department.ARTH,
+  description: `Sources and origins of artists’ video from the late 1960s to the present day; consideration of theoretical, political and technological aspects; survey of single-channel, projected, installation and Internet formats for video art display.`,
   prereqs: [],
   grade: null,
   credits: 3,
@@ -193,9 +211,10 @@ export const ARTH4482 = new Course({
 });
 
 export const ARTH4484 = new Course({
-  name: "",
+  name: "History of Electronic and Digital Art",
   code: 4484,
   department: Department.ARTH,
+  description: `Survey of art and technology focusing on the development of computer art and digital, interactive, and network-based art forms from the 1950s to the present.`,
   prereqs: [],
   grade: null,
   credits: 3,
@@ -203,9 +222,10 @@ export const ARTH4484 = new Course({
 });
 
 export const MC2035 = new Course({
-  name: "",
+  name: "Social Media and Digital Branding",
   code: 2035,
   department: Department.MC,
+  description: `Introduction to digital, mobile and social media; personal branding within the digital landscape; theoretical and practical foundations of digital media and its impact on and challenges to society.`,
   credits: 3,
   prereqs: [],
   grade: null,
@@ -213,9 +233,10 @@ export const MC2035 = new Course({
 });
 
 export const MC3031 = new Course({
-  name: "",
+  name: "Digital Advertising Creative Strategies",
   code: 3031,
   department: Department.MC,
+  description: `Introduction to advertising creative strategy development and principles of design across media; emphasis on techniques for the creation and production of digital advertising messages.`,
   prereqs: [],
   credits: 3,
   grade: null,
@@ -223,9 +244,10 @@ export const MC3031 = new Course({
 });
 
 export const MC4002 = new Course({
-  name: "",
+  name: "Strategies for Public Relations and Social Media",
   code: 4002,
   department: Department.MC,
+  description: `Exploration of strategic communication cases that use both traditional and emerging media; emphasis on ethical and practical use of social media tools to complement or supplement traditional communication strategies and tactics.`,
   credits: 3,
   prereqs: [],
   grade: null,
@@ -233,19 +255,21 @@ export const MC4002 = new Course({
 });
 
 export const MC4015 = new Course({
-  name: "",
+  name: "Advanced Visual Communication & MultiMedia Web Design",
   code: 4015,
   credits: 3,
   department: Department.MC,
+  description: `This course includes hands-on training in photo enhancement, audio refinement, and video editing, ensuring a holistic understanding of the multimedia production process.`,
   prereqs: [],
   grade: null,
   section: null,
 });
 
 export const MC4260 = new Course({
-  name: "",
+  name: "Long Format Video Production",
   code: 4260,
   department: Department.MC,
+  description: `Through a blend of theory and hands-on practice, students will explore the strategic planning, design, and production processes essential to crafting compelling video narratives.`,
   credits: 3,
   prereqs: [],
   grade: null,
@@ -253,9 +277,10 @@ export const MC4260 = new Course({
 });
 
 export const MC4550 = new Course({
-  name: "",
+  name: "Social Media Analysis & Creation",
   code: 4550,
   department: Department.MC,
+  description: `Students will be equipped with the tools and techniques to not only analyze and interpret data but also to craft content that fosters genuine connections with digital audiences.`,
   prereqs: [],
   credits: 3,
   grade: null,
@@ -263,9 +288,10 @@ export const MC4550 = new Course({
 });
 
 export const MC4600 = new Course({
-  name: "",
+  name: "Create Lab",
   code: 4600,
   department: Department.MC,
+  description: `An integrated and rigorous experience in new and emerging social media platform production. Student teams will develop a digital solution for a client`,
   prereqs: [],
   grade: null,
   credits: 3,
@@ -273,9 +299,10 @@ export const MC4600 = new Course({
 });
 
 export const MC4720 = new Course({
-  name: "",
+  name: "Television Creative Projects",
   code: 4720,
   department: Department.MC,
+  description: `Techniques of television production for non-journalism projects; includes field production, nonlinear video editing, graphics and studio production.`,
   prereqs: [],
   credits: 3,
   grade: null,
@@ -283,9 +310,10 @@ export const MC4720 = new Course({
 });
 
 export const MUS2745 = new Course({
-  name: "",
+  name: "Introduction to Computer Music",
   code: 2745,
   department: Department.MUS,
+  description: `Introduction to techniques and technologies in computer music; principles of digital audio, sound design, music synthesis, digital audio workstations, and sound art composition with an emphasis on musique concrète.`,
   prereqs: [],
   grade: null,
   credits: 3,
@@ -293,9 +321,10 @@ export const MUS2745 = new Course({
 });
 
 export const MUS4744 = new Course({
-  name: "",
+  name: "Mobile Music",
   code: 4744,
   department: Department.MUS,
+  description: `Creating music applications for mobile platforms, mobile application instrument design, mobile interaction design, experimental music composition and performance.`,
   prereqs: [],
   credits: 3,
   grade: null,
@@ -303,9 +332,10 @@ export const MUS4744 = new Course({
 });
 
 export const MUS4745 = new Course({
-  name: "",
+  name: "Computer Music",
   code: 4745,
   department: Department.MUS,
+  description: `Digital sound design, sound synthesis and signal processing; electroacoustic music composition using computers and computer music techniques`,
   prereqs: [],
   grade: null,
   credits: 3,
@@ -313,9 +343,10 @@ export const MUS4745 = new Course({
 });
 
 export const MUS4746 = new Course({
-  name: "",
+  name: "Seminar in Computer Music and Digital Media",
   code: 4746,
   department: Department.MUS,
+  description: ` Focused study of various topics in computer music and digital media such as computer music programming, sound diffusion techniques, interactive computer music and digital media systems, intermedia applications, analysis of computer music.`,
   credits: 3,
   prereqs: [],
   grade: null,
@@ -323,19 +354,21 @@ export const MUS4746 = new Course({
 });
 
 export const MUS4748 = new Course({
-  name: "",
+  name: "Digital Musical Instruments",
   code: 4748,
   credits: 3,
   department: Department.MUS,
+  description: `Digital musical instruments, physical modeling, sound design, acoustics, haptic interaction design, 21st century lutherie, and computer music composition.`,
   prereqs: [],
   grade: null,
   section: null,
 });
 
 export const ENGL2009 = new Course({
-  name: "",
+  name: "Introduction to Writing Screenplays",
   code: 2009,
   department: Department.ENGL,
+  description: `Writing screenplays for workshop criticism; techniques of exposition, characterization and dramatization.`,
   prereqs: [],
   grade: null,
   credits: 3,
@@ -343,9 +376,10 @@ export const ENGL2009 = new Course({
 });
 
 export const ENGL2231 = new Course({
-  name: "",
+  name: "Reading Film",
   code: 2231,
   department: Department.ENGL,
+  description: `Introduction to analytical study of film; mastery of film language through formal, cultural, and theoretical approaches to film study; emphasis on writing about film.`,
   prereqs: [],
   credits: 3,
   grade: null,
@@ -353,9 +387,10 @@ export const ENGL2231 = new Course({
 });
 
 export const ENGL4000 = new Course({
-  name: "",
+  name: "Secial Projects for Creative Writing Majors",
   code: 4000,
   department: Department.ENGL,
+  description: `Explorations in a wide variety of projects that basic courses are unable to accommodate.`,
   credits: 3,
   prereqs: [],
   grade: null,
@@ -363,9 +398,10 @@ export const ENGL4000 = new Course({
 });
 
 export const ENGL4009 = new Course({
-  name: "",
+  name: "Intermediate TV and Film Writing Workshop",
   code: 4009,
   department: Department.ENGL,
+  description: `Using your own scripts, you will learn to scene card or outline your scripts as well as critique each other’s work. You will watch films or TV shows of your own choosing and present a journal of your observations at end of semester. This is a workshop to complete a rough draft of your script, not a lecture course, so having fun while writing is part of the process.`,
   prereqs: [],
   grade: null,
   section: null,
@@ -373,9 +409,10 @@ export const ENGL4009 = new Course({
 });
 
 export const ENGL7109 = new Course({
-  name: "",
+  name: "Forms of Film Writing",
   code: 7109,
   department: Department.ENGL,
+  description: ` Examination of screenplays and teleplays; techniques of exposition, characterization and dramatization.`,
   prereqs: [],
   grade: null,
   credits: 3,
