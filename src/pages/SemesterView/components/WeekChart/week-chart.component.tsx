8a8db127339a72bd88c7bcd5dc4104c0874698b8
AdamASchulte <adamaschulte@yahoo.com>
2023-10-25 21:38:06 -0500

    got the words centered over the columns

--- src/pages/SemesterView/components/WeekChart/week-chart.component.tsx
+++ src/pages/SemesterView/components/WeekChart/week-chart.component.tsx
@@ -8,8 +8,14 @@ import styles from "./week-chart.module.scss";
 export const WeekChart = (): JSX.Element => {
   return (
     <>
-    <h2 className={styles.title}> Schedule </h2>
-    <h3>Mon</h3><h3>Tue</h3><h3>Wed</h3><h3>Thu</h3><h3>Fri</h3>
+      <h2 className={styles.title}> Schedule </h2>
+      <div className={styles.inlineDiv}>
+        <h3>Mon</h3>
+        <h3>Tue</h3>
+        <h3>Wed</h3>
+        <h3>Thu</h3>
+        <h3>Fri</h3>
+      </div>
       <div className={styles.grid}>
         <div className={styles.timeColumn}>7:30</div>
         <div className={styles.item}>2</div>
