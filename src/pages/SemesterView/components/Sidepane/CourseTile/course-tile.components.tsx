eb1a32efd1d6f56f640f38bc8c29bac3a8423b65
pfranz1 <pfranz1@lsu.edu>
2023-10-29 14:37:31 -0500

    wip improve information on course tiles

--- src/pages/SemesterView/components/Sidepane/CourseTile/course-tile.components.tsx
+++ src/pages/SemesterView/components/Sidepane/CourseTile/course-tile.components.tsx
@@ -6,6 +6,7 @@ import { AppState } from "../../../../../store/store";
 
 import styles from "./course-tile.module.scss";
 import { selectCourse } from "../../../../../store/Semester/semester-slice";
+import { Star } from "@mui/icons-material";
 
 export interface Props {
   course: Course;
@@ -13,6 +14,43 @@ export interface Props {
 
 export const CourseTile = ({ course }: Props): JSX.Element => {
   const dispatch = useDispatch();
+
+  let title: string;
+  let subtitle: string | null = null;
+
+  if (course instanceof CategoryCourse) {
+    title = course.name;
+
+    if (course.optionTaken) {
+      subtitle = course.optionTaken.department + " " + course.optionTaken.code;
+
+      if (course.optionTaken.section) {
+        subtitle += " - " + course.optionTaken.section.number;
+      }
+    }
+  } else {
+    title = course.department + " " + course.code;
+
+    if (course.section) {
+      subtitle = "Section: " + course.section.number;
+    } else {
+      subtitle = "Section: ???";
+    }
+  }
+
+  const subtitleObj = subtitle ? <h4>{subtitle}</h4> : <></>;
+
+  const titleObj = (
+    <div className={styles.title_container}>
+      <div className={styles.icon_container}>
+        <Star style={{ fontSize: "48px" }} />
+      </div>
+      <div className={styles.text_container}>
+        <h3>{title}</h3>
+        {subtitleObj}
+      </div>
+    </div>
+  );
 
   return (
     <div className={styles.CourseTile}>
@@ -22,16 +60,7 @@ export const CourseTile = ({ course }: Props): JSX.Element => {
         }`}
         onClick={(): unknown => dispatch(selectCourse(course))}
       >
-        <h3>
-          {course.section != null && course instanceof CategoryCourse
-            ? course.courseAbreviation + ":"
-            : ""}
-          {`${
-            course.section == null
-              ? course.courseAbreviation
-              : course.section.name
-          }`}
-        </h3>
+        <div className={styles.container}>{titleObj}</div>
       </Button>
     </div>
   );
