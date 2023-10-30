b122991f35eb6b7d8f4a004cb686554828fde0f3
pfranz1 <pfranz1@lsu.edu>
2023-10-29 21:56:30 -0500

    dynamic sizing and icons added to course-tiles

--- src/pages/SemesterView/components/Sidepane/CourseTile/course-tile.components.tsx
+++ src/pages/SemesterView/components/Sidepane/CourseTile/course-tile.components.tsx
@@ -1,12 +1,36 @@
 import React from "react";
-import { CategoryCourse, Course } from "../../../../../models/course";
+import {
+  CategoryCourse,
+  Course,
+  Department,
+} from "../../../../../models/course";
 import { Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { AppState } from "../../../../../store/store";
 
 import styles from "./course-tile.module.scss";
 import { selectCourse } from "../../../../../store/Semester/semester-slice";
-import { Star } from "@mui/icons-material";
+import {
+  AutoStories,
+  Biotech,
+  Calculate,
+  Campaign,
+  ColorLens,
+  Computer,
+  Gavel,
+  Hub,
+  MusicNote,
+  Power,
+  PrecisionManufacturing,
+  PsychologyAlt,
+  QuestionMark,
+  Science,
+  SmartToy,
+  Star,
+  Update,
+  Vrpano,
+  Work,
+} from "@mui/icons-material";
 
 export interface Props {
   course: Course;
@@ -40,13 +64,79 @@ export const CourseTile = ({ course }: Props): JSX.Element => {
 
   const subtitleObj = subtitle ? <h4>{subtitle}</h4> : <></>;
 
+  let icon;
+
+  switch (
+    course instanceof CategoryCourse
+      ? course.optionTaken?.department
+      : course.department
+  ) {
+    case Department.ART:
+      icon = <ColorLens />;
+      break;
+    case Department.ARTH:
+      icon = <Vrpano />;
+      break;
+    case Department.BA:
+      icon = <Work />;
+      break;
+    case Department.BIOL:
+      icon = <Biotech />;
+      break;
+    case Department.CHE:
+      icon = <Science />;
+      break;
+    case Department.CSC:
+      icon = <Computer />;
+      break;
+    case Department.ECE:
+      icon = <SmartToy />;
+      break;
+    case Department.EE:
+      icon = <Power />;
+      break;
+    case Department.ENGL:
+      icon = <AutoStories />;
+      break;
+    case Department.HIST:
+      icon = <Update />;
+      break;
+    case Department.IE:
+      icon = <PrecisionManufacturing />;
+      break;
+    case Department.ISDS:
+      icon = <Hub />;
+      break;
+    case Department.MATH:
+      icon = <Calculate />;
+      break;
+    case Department.MUS:
+      icon = <MusicNote />;
+      break;
+    case Department.MC:
+      icon = <Campaign />;
+      break;
+    case Department.NA:
+      icon = <QuestionMark />;
+      break;
+    case Department.PHIL:
+      icon = <PsychologyAlt />;
+      break;
+    case Department.POLS:
+      icon = <Gavel />;
+      break;
+    default:
+      icon = <QuestionMark />;
+      break;
+  }
+
   const titleObj = (
     <div className={styles.title_container}>
-      <div className={styles.icon_container}>
-        <Star style={{ fontSize: "48px" }} />
-      </div>
+      <div className={styles.icon_container}>{icon}</div>
       <div className={styles.text_container}>
-        <h3>{title}</h3>
+        <h3 className={course instanceof CategoryCourse ? styles.category : ""}>
+          {title}
+        </h3>
         {subtitleObj}
       </div>
     </div>
