404c85d388022e836d4f2529e695ae6d562db797
AdamASchulte <adamaschulte@yahoo.com>
2023-11-04 19:18:54 -0500

    small styling and stuff

--- src/pages/SemesterView/components/WeekChart/week-chart.component.tsx
+++ src/pages/SemesterView/components/WeekChart/week-chart.component.tsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AppState } from "../../../../store/store";
 import { ReactNode } from "react";
 import React from "react";
@@ -6,8 +6,16 @@ import { useState } from "react";
 import styles from "./week-chart.module.scss";
 import { start } from "repl";
 import { ScheduledSection } from "./ScheduledSection/ScheduledSection";
+import { selectCourse } from "../../../../store/Semester/semester-slice";
+import {
+  CategoryCourse,
+  Course,
+  Department,
+} from "../../../../models/course";
 
 export const WeekChart = (): JSX.Element => {
+  const dispatch = useDispatch();
+  //const { weeklySchedule } = useSelector((state) => state.)
   const [scheduledSection, setScheduleSection] = useState([
     { courseName: "CSC 4243", rowStart: 0, rowEnd: 1, colStart: 1, colEnd: 2 },
     { courseName: "CSC 4402", rowStart: 0, rowEnd: 1, colStart: 2, colEnd: 3 },
@@ -27,21 +35,21 @@ export const WeekChart = (): JSX.Element => {
         <h3>Thu</h3>
         <h3>Fri</h3>
       </div>
-      <div className={styles.grid}>
-        {scheduledSection.map((item) => (
-          <ScheduledSection
-            key={item.courseName}
-            courseName={item.courseName}
-            rowStart={item.rowStart}
-            rowEnd={item.rowEnd}
-            colStart={item.colStart}
-            colEnd={item.colEnd}
-            onClick={(): number => 5 + 5}
-          >
-            {item.courseName}
-          </ScheduledSection>
-        ))}
-      </div>
+        <div className={styles.grid}>
+          {scheduledSection.map((item) => (
+            <ScheduledSection
+              key={item.courseName}
+              courseName={item.courseName}
+              rowStart={item.rowStart}
+              rowEnd={item.rowEnd}
+              colStart={item.colStart}
+              colEnd={item.colEnd}
+              onClick={(): unknown => 5 + 5}
+            >
+              {item.courseName}
+            </ScheduledSection>
+          ))}
+        </div>
     </>
   );
 };
