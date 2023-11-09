14b63a738aa90122d71ad98b16b443506bdb3757
Aaron Cheng <acheng8@lsu.edu>
2023-11-08 21:07:38 -0600

    Started doing the user info bototm bar stuff

--- src/components/BottomBar/bottombar.component.tsx
+++ src/components/BottomBar/bottombar.component.tsx
@@ -1,11 +1,27 @@
 import React, { Fragment } from "react";
 import styles from "./bottombar.module.scss";
+import { IconButton } from "@mui/material";
+import { AccountBox } from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import { AppState } from "../../store/store";
 
 const BottomBar = (): JSX.Element => {
+  const $student = useSelector((state: AppState) => state.student);
+
   return (
     <Fragment>
-      <div className={styles.BottomBar}></div>
+      <div className={styles.BottomBar}>
+        <h2 className={styles.name}>
+          {$student.firstName}{" "}
+          {$student.middleName ? $student.middleName[0] : ""}{" "}
+          {$student.lastName}{" "}
+        </h2>
+        <IconButton className={styles.profileButton}>
+          <AccountBox />
+        </IconButton>
+      </div>
     </Fragment>
   );
 };
+
 export default BottomBar;
